134d0b441df2717c2c7aeeb306e3d8ad4e4dc5be
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-08-19 21:09:13 +0100

    update: clean up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
