04db85017c50063bc6d4c2ac293e0493cafae373
Emmanuel Michael <michochoprogrammer@gmail.com>
2024-08-22 00:00:43 +0100

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-import variables from './variables.modules.scss';
+// import Image from "next/image";
+import styles from './page.modules.sass';
 
 export default function Home() {
   return (
     <main>
-      <h1 style={{color: variables.primaryColor, fontSize: '1rem'}}>Home Page</h1>
+      <h1 className={styles.main_heading}>Home Page</h1>
     </main>
   );
 }
--- scss.d.ts
+++ scss.d.ts
@@ -2,3 +2,8 @@ declare module '*.scss' {
   const content: { [className: string]: string };
   export default content;
 }
+
+declare module '*.sass' {
+  const content: { [className: string]: string };
+  export default content;
+}
